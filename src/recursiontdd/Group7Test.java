531f1ad5fd92c40fb1806c98841db4c69326890d
samliu12 <sam.liu21@ycdsbk12.ca>
2020-12-02 15:07:45 +0000

    made more tests

--- src/recursiontdd/Group7Test.java
+++ src/recursiontdd/Group7Test.java
@@ -17,8 +17,16 @@ public class Group7Test {
   public void Test1() {
     // make assertion statement(s)
     //assertEquals(EXPECTEDVALUE, yourClassToTest.methodToTest());
-    assertEquals(true, Group7.array6(new int[]{6, 5, 9, 7}, 0));
+    assertEquals(true, Group7.array6(new int[] {6, 5, 9, 7}, 0));
   }
 
+  @Test
+  public void Test2() {
+    assertFalse(Group7.array6(new int[] {5, 5, 3, 2}));
+  }
   
+  @Test
+  public void Test3() {
+    assertEquals(true, Group7.array6(new int[] {3, 6, 1, 8}, 0));
+  }
 }
