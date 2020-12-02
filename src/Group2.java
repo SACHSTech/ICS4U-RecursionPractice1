24cc51656c26be14350dc48dd6225f1169c0d6a8
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-12-02 14:34:16 +0000

    Added base case

---
+++ src/Group2.java
@@ -0,0 +1,15 @@
+public class Group2 {
+
+  public static int sumDigits(int n) {
+    if (n < 10) {
+      return n;
+    }
+    return n;
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sumDigits(123));
+  }
+
+}
