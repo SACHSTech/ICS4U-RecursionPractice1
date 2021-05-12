03727ec662dade39032ae815631bffefe2ad47a2
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-05-12 09:09:08 -0400

    Update and rename Group2Test.java to TestTemplateTest.java

--- src/recursiontdd/TestTemplateTest.java
+++ src/recursiontdd/TestTemplateTest.java
@@ -2,12 +2,13 @@ package recursiontdd;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import recursiontdd.Group2;
+import recursiontdd.GroupNUM; //rename to reference proper class
 
 /**
 * See https://github.com/mrfabroa/Fabroa-ICS4U-NotesExamples/blob/main/src/tdd/TDDExampleTest.java for an example
 */
-public class Group2Test {
+
+public class GroupNUMTest { // rename class to refere proper class
 
     /**
      * An initial test
@@ -15,13 +16,8 @@ public class Group2Test {
     @Test
     public void Test1(){
         // make assertion statement(s)
-        assertEquals(1, Group2.sumDigits(1));
     }
 
-    @Test
-    public void Test2(){
-        // make assertion statement(s)
-        assertEquals(6, Group2.sumDigits(123));
-    }
+   //Add additional tests
   
 }
